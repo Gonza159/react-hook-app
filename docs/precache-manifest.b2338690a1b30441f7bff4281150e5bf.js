5fc3c1a9b69caac7b18f5994aaa2a26ed1d8b9df
Gonzalo S <gonzaa159@gmail.com>
2020-07-28 16:25:24 -0300

    first commit

---
+++ docs/precache-manifest.b2338690a1b30441f7bff4281150e5bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbed856797dc1ba52e2f33652446274e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75eb81a133b14fb3cfe2",
+    "url": "/static/css/main.cfb3722e.chunk.css"
+  },
+  {
+    "revision": "9609f607b7f67dafefa3",
+    "url": "/static/js/2.86c83d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86c83d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75eb81a133b14fb3cfe2",
+    "url": "/static/js/main.ee95e5b2.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
